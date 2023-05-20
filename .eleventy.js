8880dd15ed6006d37ca8aa003a1c854d657ce8c7
Miriam Suzanne <miriam@oddbird.net>
2023-05-20 01:26:26 -0600

    Ratios and gearing

--- .eleventy.js
+++ .eleventy.js
@@ -6,8 +6,10 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/_css': 'css' });
 
-  eleventyConfig.addPlugin(pluginWebc);
 	eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_components/**/*.webc",
+  });
 
 	eleventyConfig.setLiquidOptions({jsTruthy: true});
   eleventyConfig.setQuietMode(true);
